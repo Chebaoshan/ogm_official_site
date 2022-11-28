b4ba77d037895e4de92ace370077f625a396e587
chebaoshan <chebaoshan2022@outlook.com>
2022-11-28 23:03:00 +0900

    20221128

--- new/src/App.vue
+++ new/src/App.vue
@@ -7,7 +7,10 @@
     <div class="l-all-wrapper js-all-wrapper--top">
         <commonHeader></commonHeader>
         <div class="l-all-wrapper_inner s-top-noinfo">
-            <router-view>
+            <router-view v-slot="{ Component }">
+                <transition name="slide-fade" mode="out-in" :duration="{ enter: 500, leave: 300 }">
+                    <component :is="Component" />
+                </transition>
             </router-view>
             <commonFooter></commonFooter>
         </div>
@@ -17,6 +20,20 @@
 import commonHeader from './view/commonHeader.vue'
 import commonFooter from './view/commonFooter.vue'
 </script>
-<style>
+<style la="less">
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
 
+.slide-fade-leave-active {
+  transition: all 0.5s;
+}
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  opacity: 0.3;
+}
+
+.slide-fade-enter-to, .slide-fade-leave-from {
+  opacity: 1;
+}
 </style>
